76f816e27451126c4303bff96fed16d441adae4b
Jaime Rodas <jaime@surrealista.mx>
2016-03-09 21:04:18 -0600

    Ya guardamos a dropbox

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
+var parseString = require('xml2js').parseString;
+
 var app = express();
 
 var config = require('./config.json');
 
 app.set('port', (config.port || 5000));
+
+var defineFolder = function(date) {
+    var mes = date.getMonth() + 1;
+    mes = mes[1] ? mes : '0' + mes;
+    return '/' + date.getFullYear() + '-' + mes;
+};
+
+var definePath = function(date, rfc, extension, folder) {
+    return folder + '/' + Math.round(date.getTime()/1000, 0) + '-' + rfc + '.' + extension;
+};
 
 var crearFolder = function (folder) {
     request({
@@ -21,22 +33,71 @@ var crearFolder = function (folder) {
     });
 };
 
+var crearArchivo = function (bin, path) {
+    request({
+        method: 'POST',
+        uri: 'https://content.dropboxapi.com/2/files/upload',
+        headers: {
+            'Authorization': 'Bearer ' + config.github_key,
+            'Dropbox-API-Arg': JSON.stringify({
+                path: path,
+                autorename: true
+            }),
+            'Content-type': 'application/octet-stream'
+        },
+        body: bin
+    }, function (e, r, b) {
+        console.log(b);
+    });
+};
+
 app.use(bodyParser.json({limit: '10mb'}));
 
 app.post('/recibe', function (req, res) {
-    console.log(req.body);
+    // console.log(req.body);
 
-    fecha = new Date(req.body.Date);
-    mes = fecha.getMonth() + 1;
+    if (req.body.Attachments.length > 0) {
+        var fecha, rfc, folder,
+        save = [];
 
-    if (mes.toString().length == 1) {
-        mes = '0' + mes;
+        req.body.Attachments.forEach(function(att){
+            var file, text;
+
+            if (att.ContentType == 'text/xml') {
+                file = new Buffer(att.Content, 'base64');
+                text = file.toString();
+
+                save.push({
+                    extension: 'xml',
+                    bin: file
+                });
+
+                parseString(text, function (err, result) {
+                    fecha = new Date(result['cfdi:Comprobante'].$.fecha);
+                    rfc = result['cfdi:Comprobante']['cfdi:Emisor'][0].$.rfc;
+                });
+            }
+
+            if (att.ContentType == 'application/pdf') {
+                file = new Buffer(att.Content, 'base64');
+
+                save.push({
+                    extension: 'pdf',
+                    bin: file
+                });
+            }
+        });
+
+        if (fecha && rfc) {
+            folder = defineFolder(fecha);
+            crearFolder(folder);
+
+            save.forEach(function(a){
+                var path = definePath(fecha, rfc, a.extension, folder);
+                crearArchivo(a.bin, path);
+            });
+        }
     }
-
-    folder = '/' + fecha.getFullYear() + '-' + mes;
-
-    // Creamos el folder
-    crearFolder(folder);
 
     res.status(200).end();
 });
